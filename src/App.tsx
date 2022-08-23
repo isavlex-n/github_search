96d7520132731cf7fa3c2f34b5db2b95c737d197
Alexander Senin <isavlex@yandex.ru>
2022-08-23 21:30:29 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Navigation } from './components/Navigation'
+import { FavoritePage } from './pages/FavoritePage'
+import { HomePage } from './pages/HomePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/favorites' element={ <FavoritePage /> } />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/FavoritePage.tsx
@@ -0,0 +1,18 @@
+import { useAppSelector } from '../hooks/redux'
+
+export const FavoritePage = () => {
+  const { favorites } = useAppSelector((state) => state.github)
+
+  if (favorites.length === 0) return <p className="text-center">No items</p>
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        {favorites.map((f) => (
+          <li key={f}>
+            <a href={f} target="blank">{f}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
